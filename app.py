9c4e37dc16e8422b0732a1ec7e0fe3fd054b733a
miguelito440 <162323702+miguelito440@users.noreply.github.com>
2026-05-28 17:12:04 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request, redirect, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = 'chave_super_secreta_do_grupo' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///biblioteca.db'
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    senha_hash = db.Column(db.String(200), nullable=False)
+
+@app.route('/')
+def tela_inicial():
+    return render_template('login.html')
+
+@app.route('/cadastrar', methods=['POST'])
+def cadastrar():
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    senha = request.form.get('senha')
+
+    if len(senha) < 6:
+        flash('Ops! Sua senha precisa ter pelo menos 6 caracteres.')
+        return redirect('/')
+
+    usuario_existente = Usuario.query.filter_by(email=email).first()
+    if usuario_existente:
+        flash('Este e-mail já está cadastrado. Tente fazer login!')
+        return redirect('/')
+
+    senha_criptografada = generate_password_hash(senha)
+    novo_usuario = Usuario(nome=nome, email=email, senha_hash=senha_criptografada)
+    db.session.add(novo_usuario)
+    db.session.commit()
+    
+    flash('Cadastro realizado com sucesso! Agora é só entrar.')
+    return redirect('/')
+
+@app.route('/login', methods=['POST'])
+def fazer_login():
+    email = request.form.get('email')
+    senha = request.form.get('senha')
+
+    usuario = Usuario.query.filter_by(email=email).first()
+
+    if usuario and check_password_hash(usuario.senha_hash, senha):
+        session['usuario_id'] = usuario.id 
+        return redirect('/catalogo')
+    else:
+        flash('E-mail ou senha incorretos. Tente novamente!')
+        return redirect('/')
+
+@app.route('/catalogo')
+def catalogo():
+    if 'usuario_id' not in session:
+        flash('Ei! Você precisa fazer login antes de ver os livros.')
+        return redirect('/')
+    
+    return render_template('catalogo.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('usuario_id', None)
+    flash('Você saiu do sistema com segurança.')
+    return redirect('/')
+
+@app.route('/meus_livros')
+def meus_livros():
+    # Proteção de Rota
+    if 'usuario_id' not in session:
+        flash('Ei! Você precisa fazer login antes de ver seus livros.')
+        return redirect('/')
+    
+    return render_template('meus_livros.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all() 
+    app.run(debug=True)
